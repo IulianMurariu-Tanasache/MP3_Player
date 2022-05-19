9b91e74d4ccd5f7845a45f6c7daae11a8e4da41e
IulianMurariu-Tanasache <iulian.murariutanasache@gmail.com>
2022-05-19 21:22:10 +0300

    unit teste: 6

---
+++ UnitTestPlaylistModule/UnitTestPlaylistManager.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PlaylistControls;
+
+namespace UnitTestPlaylistModule
+{
+    [TestClass]
+    public class UnitTestPlaylistManager
+    {
+        [TestMethod]
+        public void TestAddPlaylist()
+        {
+            PlaylistManager playlistManager = new PlaylistManager();
+            Assert.IsNotNull(playlistManager);
+            Playlist playlist1 = new Playlist("playlist1");
+            Playlist playlist2 = new Playlist("playlist2");
+            playlistManager.AddPlaylist(playlist1);
+            Assert.AreSame(playlist1, playlistManager.Playlists[0]);
+            playlistManager.AddPlaylist(playlist2);
+            Assert.AreSame(playlist2, playlistManager.Playlists[1]);
+        }
+
+        [TestMethod]
+        public void TestRemovePlaylist()
+        {
+            PlaylistManager playlistManager = new PlaylistManager();
+            Assert.IsNotNull(playlistManager);
+            Playlist playlist1 = new Playlist("playlist1");
+            Playlist playlist2 = new Playlist("playlist2");
+            playlistManager.AddPlaylist(playlist1);
+            playlistManager.AddPlaylist(playlist2);
+            playlistManager.RemovePlaylist(playlist1.Name);
+            Assert.AreEqual(1, playlistManager.Playlists.Count);
+            Assert.IsFalse(playlistManager.Playlists.Contains(playlist1));
+        }
+    }
+}
